800da8f1c8b62b13fc82d9ded1bc1b81a769f0cf
samrateth <38979259+samrateth@users.noreply.github.com>
2023-07-28 15:40:22 +0200

    Key API calls and Webs streaming socket

--- kiteConnectAPI/1_account_authorization/scripts/kc_orders.py
+++ kiteConnectAPI/1_account_authorization/scripts/kc_orders.py
@@ -29,23 +29,61 @@ def placeMarketOrder(symbol,buy_sell,quantity):
                     order_type=kite.ORDER_TYPE_MARKET,
                     product=kite.PRODUCT_MIS,
                     variety=kite.VARIETY_REGULAR)
-    
+
+placeMarketOrder("ACC", "buy", 1)    
 
     
-def placeBracketOrder(symbol,buy_sell,quantity,atr,price):    
-    # Place an intraday market order on NSE
+#def placeBracketOrder(symbol,buy_sell,quantity,atr,price):    
+#    # Place an intraday market order on NSE
+#    if buy_sell == "buy":
+#        t_type=kite.TRANSACTION_TYPE_BUY
+#    elif buy_sell == "sell":
+#        t_type=kite.TRANSACTION_TYPE_SELL
+#    kite.place_order(tradingsymbol=symbol,
+#                    exchange=kite.EXCHANGE_NSE,
+#                    transaction_type=t_type,
+#                   quantity=quantity,
+#                  order_type=kite.ORDER_TYPE_LIMIT,
+#                 price=price, #BO has to be a limit order, set a low price threshold
+#                    product=kite.PRODUCT_MIS,
+#                    variety=kite.VARIETY_BO,
+#                    squareoff=int(6*atr), 
+#                    stoploss=int(3*atr), 
+#                    trailing_stoploss=2)
+
+
+def placeCoverOrder(symbol,buy_sell,quantity,trigger):
+# Place a cover order on NSE
+# trigger variable is the price at which you want the 
     if buy_sell == "buy":
         t_type=kite.TRANSACTION_TYPE_BUY
     elif buy_sell == "sell":
         t_type=kite.TRANSACTION_TYPE_SELL
     kite.place_order(tradingsymbol=symbol,
-                    exchange=kite.EXCHANGE_NSE,
-                    transaction_type=t_type,
-                    quantity=quantity,
-                    order_type=kite.ORDER_TYPE_LIMIT,
-                    price=price, #BO has to be a limit order, set a low price threshold
-                    product=kite.PRODUCT_MIS,
-                    variety=kite.VARIETY_BO,
-                    squareoff=int(6*atr), 
-                    stoploss=int(3*atr), 
-                    trailing_stoploss=2)
+                     exchange=kite.EXCHANGE_NSE,
+                     transaction_type=t_type,  
+                     quantity=quantity,
+                     order_type=kite.ORDER_TYPE_MARKET,
+                     product=kite.PRODUCT_MIS,
+                     variety=kite.VARIETY_CO,
+                     trigger_price = trigger)
+    
+  
+
+   
+
+"""
+Zerodha Kite Connect Intro - Bibliography
+
+product=kite.PRODUCT_MIS (use this for intraday trading)
+CNC = Cash and Carry. Gives no leverage and used to buy stocks
+MIS = Margin Intraday Square off. Used for intraday trading and the open positions are automatically squared off at the end of the day
+NRML = Normal for F&O. Used to carry over the trade to next days till the expiry. However, margin is not provided for such trades.
+
+
+order_type=kite.ORDER_TYPE_MARKET 
+Market Order= Order at market price
+Limit Order= 
+
+"""    
+    
